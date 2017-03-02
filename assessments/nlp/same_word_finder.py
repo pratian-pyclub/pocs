6b54599b545758109592b6b0a47c10bca76c2810
swaathi <swaathi@skcript.com>
2017-03-02 15:45:18 +0530

    Adding Assessments and Assignments

---
+++ assessments/nlp/same_word_finder.py
@@ -0,0 +1,17 @@
+from __future__ import division
+from nltk.corpus import wordnet as wn
+import sys
+
+def similarity(w1, w2, sim=wn.path_similarity):
+  synsets1 = wn.synsets(w1)
+  synsets2 = wn.synsets(w2)
+  sim_scores = []
+  for synset1 in synsets1:
+    for synset2 in synsets2:
+      sim_scores.append(sim(synset1, synset2))
+  if len(sim_scores) == 0:
+    return 0
+  else:
+    return max(sim_scores)
+
+print similarity('trash', 'waste')
